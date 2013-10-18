328cb83880a8f3e15c63d9503728ef3b522c01b3
RendaniTshinakaho <r.nkhumeleni@gmail.com>
2013-10-18 15:24:42 +0200

    Completed ... was easy enough :)

--- FizzBuzz.Solution/Domain.Test/Test.cs
+++ FizzBuzz.Solution/Domain.Test/Test.cs
@@ -5,10 +5,59 @@ namespace Domain.Test
     [TestFixture]
     public class Test
     {
+        private IFizzBuzz _fizzBuzz;
+        [SetUp]
+        public void SetUp()
+        {
+           _fizzBuzz = new FizzBuzz(); 
+        }
          [Test]
-         public void CanCreateClass()
+         public void OneReturnOne()
          {
-             Assert.That(new FizzBuzz(),Is.Not.Null);
+             var result = _fizzBuzz.Evaluate(1);
+             Assert.That(result,Is.EqualTo("1"));
          }
+        [Test]
+        public void TwoReturnTwo()
+        {
+            var result = _fizzBuzz.Evaluate(2);
+            Assert.That(result,Is.EqualTo("2"));
+        }
+        [Test]
+        public void ThreeReturnsFizz()
+        {
+            var result = _fizzBuzz.Evaluate(3);
+            Assert.That(result,Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void FourReturnFour()
+        {
+            var result = _fizzBuzz.Evaluate(4);
+            Assert.That(result,Is.EqualTo("4"));
+        }
+        [Test]
+        public void FiveReturnBuzz()
+        {
+            var result = _fizzBuzz.Evaluate(5);
+            Assert.That(result,Is.EqualTo("Buzz"));
+        }
+        [Test]
+        public void SixthReturnFizz()
+        {
+            var result = _fizzBuzz.Evaluate(6);
+            Assert.That(result,Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void TenReturnBuzz()
+        {
+            var result = _fizzBuzz.Evaluate(10);
+            Assert.That(result,Is.EqualTo("Buzz"));
+        }
+        [Test]
+        public void FifteenReturnFizzBuzz()
+        {
+            var result = _fizzBuzz.Evaluate(15);
+            Assert.That(result,Is.EqualTo("FizzBuzz"));
+        }
     }
 }
--- FizzBuzz.Solution/Domain/FizzBuzz.cs
+++ FizzBuzz.Solution/Domain/FizzBuzz.cs
@@ -1,7 +1,36 @@
 ﻿namespace Domain
 {
-    public class FizzBuzz
+    public interface IFizzBuzz
     {
-         
+        string Evaluate(int number);
+    }
+
+    public class FizzBuzz : IFizzBuzz
+    {
+        public string Evaluate(int number)
+        {
+            if (MultipleOfThree(number) && MultipleOfFive(number))
+            {
+                return "FizzBuzz";
+            }
+            if (MultipleOfThree(number))
+            {
+                return "Fizz";
+            }else if (MultipleOfFive(number))
+            {
+                return "Buzz";
+            }
+            return number.ToString();
+        }
+
+        private bool MultipleOfFive(int number)
+        {
+            return number % 5 == 0;
+        }
+
+        private bool MultipleOfThree(int number)
+        {
+            return number % 3 == 0;
+        }
     }
 }
