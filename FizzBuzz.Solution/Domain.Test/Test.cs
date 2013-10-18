4d77e972d6d4bc57513c064a9a966c9379c2077a
RendaniTshinakaho <r.nkhumeleni@gmail.com>
2013-10-18 10:52:48 +0200

    added nunit

--- FizzBuzz.Solution/Domain.Test/Test.cs
+++ FizzBuzz.Solution/Domain.Test/Test.cs
@@ -1,7 +1,14 @@
-﻿namespace Domain.Test
+﻿using NUnit.Framework;
+
+namespace Domain.Test
 {
+    [TestFixture]
     public class Test
     {
-         
+         [Test]
+         public void CanCreateClass()
+         {
+             Assert.That(new FizzBuzz(),Is.Not.Null);
+         }
     }
 }
